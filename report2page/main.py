5e48a2b5606c5952ac7e0310be0939026a9a5770
Rob <robert.cook@bcu.ac.uk>
2022-03-24 11:44:11 +0000

    Add css styling

--- report2page/main.py
+++ report2page/main.py
@@ -1,20 +1,39 @@
+from dominate import document, tags
+import re
+
 from .configparse import ConfigParse
 from .filespider import FileSpider
-from dominate import document, tags
+
+strip_type = lambda s: re.sub(".html$", "", s)
+
 
 def main(path):
-
     cp = ConfigParse(path)
     paths = cp.paths
 
     doc = document(title="Generated pages")
-    with doc:
-        for folder in paths:
-            tags.h1(folder)
-            fs = FileSpider(folder)
 
-            for page in fs.html_pages:
-                tags.p(tags.code(tags.a(page, href=f"{folder}/{page}")))
+    with doc.head:
+        tags.link(rel='stylesheet', href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css")
+        tags.link(rel='stylesheet', href="bootstrap-toc.css")
+        tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/v4-shims.min.css')
+        tags.link(rel='stylesheet', href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css')
+
+    with doc:
+        with tags.div(cls="container template-reference-index"):
+            with tags.div(cls="row"):
+                with tags.table(cls="ref-index"):
+                    for folder in paths:
+                        with tags.tbody():
+                            tags.tr(tags.h2(folder))
+
+                        fs = FileSpider(folder)
+
+                        for page in fs.html_pages:
+                            with tags.tr():
+                                tags.td(tags.p(tags.code(tags.a(
+                                    strip_type(page),
+                                    href=f"{folder}/{page}"))))
 
     with open("index.html", "w") as f:
         f.write(str(doc))
